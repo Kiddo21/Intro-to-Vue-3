65d12561ec8a45a96110b02da1473f8abf10810c
Ferdynand Oshomegie <kelvinoshomegie24@gmail.com>
2023-07-19 16:45:19 +0100

    creating a vue app and then displaying products in the html document

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp ({
+    data() {
+        return{
+            product: 'Socks',
+            proddescription: 'A soft feeling material made to be worn on the foot for comfort.'
+        }
+    }
+})
